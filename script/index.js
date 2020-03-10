25eecc425f838338e9e7a9e67ff8445f68e3b945
Lovepreet1995 <59740667+Lovepreet1995@users.noreply.github.com>
2020-03-10 15:26:17 -0400

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,22 @@
+//TASK 1
+
+let tag = document.getElementById("tag-line");
+let headings = document.querySelectorAll(".bg-main-content h2");
+let collect = `${tag.textContent}\n`;
+for(let heading of headings){
+    collect += `\t${heading.textContent}\n`; 
+}
+
+alert(collect);
+
+
+//TASK 2
+
+let bxs = document.querySelectorAll('.bg-main-content .box');
+let when_to_launch = bxs[12];
+let scoopedElements = when_to_launch.children;
+
+let collecti= `${scoopedElements[0].innerText}\n\n${scoopedElements[1].innerText}\n\n${scoopedElements[2].innerText}`
+
+alert(collecti);
+
